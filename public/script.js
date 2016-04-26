e8b46f7572c860982f0aab3ad0418a581f824d30
Sam Lord <sal52@kent.ac.uk>
2016-04-26 19:36:12 +0100

    Refactor client a bit

--- public/script.js
+++ public/script.js
@@ -4,6 +4,18 @@ const connectionString = (window.location.hostname === 'localhost')
   ? 'ws://localhost:8080'
   : 'ws://' + window.location.host + ':8000'
 
+const chat = {
+  changeRoom: (room) => {
+    state.room = room
+    sendJson({room})
+  },
+  changeName: (name) => {
+    state.name = name
+    sendJson({name})
+  },
+  send: (content) => sendJson({content})
+}
+
 const state = {
   ws: new window.WebSocket(connectionString),
   name: '',
@@ -17,18 +29,8 @@ state.ws.onmessage = (json) => {
     : console.log('Anonymous: ' + message.content)
 }
 
-const sendJson = (json) => state.ws.send(JSON.stringify(json))
-
-const chat = {
-  changeRoom: (room) => {
-    state.room = room
-    sendJson({room})
-  },
-  changeName: (name) => {
-    state.name = name
-    sendJson({name})
-  },
-  send: (content) => sendJson({content})
+state.ws.onconnect = () => {
+  chat.changeRoom('default')
 }
 
-chat.changeRoom('default')
+const sendJson = (json) => state.ws.send(JSON.stringify(json))
